55cf492c21ae8b64d704c8bbc0db92781a898cf1
achiewe <mamukashviliachi706@gmail.com>
2023-10-16 17:19:17 +0400

    create header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { League_Spartan } from "next/font/google";
+import Header from "./components/Header";
 
 const spartan = League_Spartan({ subsets: ["latin"], weight: ["500", "700"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={spartan.className}>{children}</body>
+      <body className={spartan.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold text-black">Hello world!</h1>;
+  return <div> </div>;
 }
