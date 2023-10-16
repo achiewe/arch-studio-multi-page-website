c42cb37b2d559f93e030ccc6e1d0fab4620341c8
achiewe <mamukashviliachi706@gmail.com>
2023-10-16 17:52:20 +0400

    import svg from public folder and add some file

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,9 @@
+import logoSvg from "../../../public/assets/logo.svg";
+
 export default function Header(): JSX.Element {
-  return <header> </header>;
+  return (
+    <header className="flex flex-row p-[32px] justify-between w-full items-center">
+      <img src={logoSvg} alt="logo svg" />
+    </header>
+  );
 }
