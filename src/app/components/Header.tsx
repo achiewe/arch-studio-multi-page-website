dcda04db06512a50b1117dfce6154f8908b5b384
achiewe <mamukashviliachi706@gmail.com>
2023-10-17 12:19:20 +0400

    add header component svgs

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,11 @@
 import logoSvg from "../../../public/assets/logo.svg";
+import burgerSvg from "../../../public/assets/icons/icon-hamburger.svg";
 
 export default function Header(): JSX.Element {
   return (
     <header className="flex flex-row p-[32px] justify-between w-full items-center">
-      <img src={logoSvg.src} alt="logo svg" />
+      <img src={logoSvg.src} alt="logo svg" className="w-[77px] h-[32.028px]" />
+      <img src={burgerSvg.src} alt="burger svg" />
     </header>
   );
 }
