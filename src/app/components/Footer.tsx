b6f49a64cfdb5da39f9cf20451a588f06db32fa7
achiewe <mamukashviliachi706@gmail.com>
2023-10-18 21:26:42 +0400

    import new svg from assets and add it in footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,9 @@
+import archSvg from "../../../public/assets/Group 21.svg";
+
 export default function Footer(): JSX.Element {
   return (
-    <div className="bg-[#EEEFF4] w-full flex flex-col items-center gap-[33px]"></div>
+    <div className="bg-[#EEEFF4] w-full flex flex-col items-center gap-[33px]">
+      <img src={archSvg.src} alt="arch svg" className="mt-[-60px]" />
+    </div>
   );
 }
