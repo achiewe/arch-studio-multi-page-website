e1ef85974f30b2fa6c003033828552fa1b58c04e
achiewe <mamukashviliachi706@gmail.com>
2023-10-19 23:23:43 +0400

    add footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import archSvg from "../../../public/assets/Group 21.svg";
 import Link from "next/link";
 
+//add footer component
 export default function Footer(): JSX.Element {
   return (
     <div className="bg-[#EEEFF4] w-full flex flex-col items-center gap-[33px] pb-[48px]">
