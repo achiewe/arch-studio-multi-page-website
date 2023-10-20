521bac0cf9ee92842b04417574ff6263595f407b
achiewe <mamukashviliachi706@gmail.com>
2023-10-20 21:23:47 +0400

    add bg url

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -1,5 +1,5 @@
 export default function ProjectParamouInfo(): JSX.Element {
   return (
-    <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-[url('../../../../public/home/mobile/image-hero-paramour.jpg')]"></div>
+    <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-[url('')]"></div>
   );
 }
