8481e9b450c6557c8e4822c032b78e489a0aad57
achiewe <mamukashviliachi706@gmail.com>
2023-10-20 21:10:43 +0400

    add css styles for div

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -1,3 +1,5 @@
 export default function ProjectParamouInfo(): JSX.Element {
-  return <div></div>;
+  return (
+    <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px]"></div>
+  );
 }
