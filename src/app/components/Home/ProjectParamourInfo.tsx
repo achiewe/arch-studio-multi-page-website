08c4ed69d54e75ebe920109bb5b1a1fe34726ac6
achiewe <mamukashviliachi706@gmail.com>
2023-10-21 20:00:04 +0400

    add comment about home component

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -1,3 +1,5 @@
+// create paramcomponent for home page
+
 export default function ProjectParamouInfo(): JSX.Element {
   return (
     <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-[url('')]"></div>
