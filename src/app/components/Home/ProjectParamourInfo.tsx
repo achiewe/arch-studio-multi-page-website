3c285f709e9768999a3ce17419ab6a819eefeba6
achiewe <mamukashviliachi706@gmail.com>
2023-10-22 14:09:56 +0400

    correct background image

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -2,6 +2,6 @@
 
 export default function ProjectParamouInfo(): JSX.Element {
   return (
-    <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-[url('/public/assets/home/mobile/image-hero-paramour.jpg')]"></div>
+    <div className="w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-[url('./public/assets/home/mobile/image-hero-paramour.jpg')]"></div>
   );
 }
