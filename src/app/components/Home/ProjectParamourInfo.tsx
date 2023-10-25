e7f4aa95bd48bc719fbf99e883c03d6bb89dda75
achiewe <mamukashviliachi706@gmail.com>
2023-10-25 20:02:23 +0400

    add paramour jpg inside of div

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -1,9 +1,10 @@
 // create paramcomponent for home page
+import paramourJpg from "../../../../public/assets/home/mobile/image-hero-paramour.jpg";
 
 export default function ProjectParamourInfo(): JSX.Element {
   return (
-    <div
-      className={`w-full h-[560px] flex px-[32px] pt-[116px] pb-[110px] bg-heroPattern bg-cover`}
-    ></div>
+    <div className={`w-full h-[560px] flex bg-heroPattern`}>
+      <img src={paramourJpg.src} alt="paramour jpg" />
+    </div>
   );
 }
