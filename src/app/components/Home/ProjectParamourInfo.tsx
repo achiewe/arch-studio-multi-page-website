f77726ea06443f939fc80e3425115cd3835d7552
achiewe <mamukashviliachi706@gmail.com>
2023-10-26 19:17:40 +0400

    add title about project paramour

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -8,8 +8,14 @@ export default function ProjectParamourInfo(): JSX.Element {
 
   return (
     <div
-      className="w-full h-[560px] flex bg-heroPattern bg-no-repeat bg-cover"
+      className="w-full h-[560px] flex bg-heroPattern bg-no-repeat bg-cover px-[32px] justify-center items-center flex-col gap-[83px]"
       style={styleBack}
-    ></div>
+    >
+      <div className="w-full flex flex-col gap-[11px] items-start">
+        <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px]">
+          Project Paramour
+        </h1>
+      </div>
+    </div>
   );
 }
