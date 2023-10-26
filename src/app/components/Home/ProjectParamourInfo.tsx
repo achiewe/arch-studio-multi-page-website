b9e7214b7959b8f0aa5e0138eee6a25f7afb8709
achiewe <mamukashviliachi706@gmail.com>
2023-10-26 19:08:22 +0400

    add background image in varibale

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -2,9 +2,9 @@
 import paramourJpg from "../../../../public/assets/home/mobile/image-hero-paramour.jpg";
 
 export default function ProjectParamourInfo(): JSX.Element {
-  return (
-    <div className={`w-full h-[560px] flex bg-heroPattern`}>
-      <img src={paramourJpg.src} alt="paramour jpg" className="w-full" />
-    </div>
-  );
+  const styleBack = {
+    backgroundImg: `url(${paramourJpg.src})`,
+  };
+
+  return <div className={`w-full h-[560px] flex bg-heroPattern `}></div>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,6 @@ export default function RootLayout({
       <Providers>
         <body className={spartan.className}>
           <Header />
-
-          <div className="w-full min-h-screen bg-[#000000] opacity-50 absolute"></div>
           <BurgerMenu />
           {children}
 
