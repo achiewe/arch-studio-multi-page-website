5da4a4a11dd07b3f95c06c88fad47dff68af8ecf
achiewe <mamukashviliachi706@gmail.com>
2023-10-27 17:23:44 +0400

    add div for inside of it create background image

--- src/app/components/Home/FeaturedComp.tsx
+++ src/app/components/Home/FeaturedComp.tsx
@@ -4,6 +4,9 @@ export default function FeaturedComp() {
       <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[-1.714px]">
         Featured
       </h1>
+      <div className="w-full flex flex-col gap-[24px] items-center justify-center">
+        <div className="w-full h-[240px] pl-[24px] pb-[23px] flex justify-end"></div>
+      </div>
     </div>
   );
 }
