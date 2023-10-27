f82b545b89cac9e1734aca873dba00d6558d8081
achiewe <mamukashviliachi706@gmail.com>
2023-10-27 17:12:10 +0400

    create freature Comp for home page

--- src/app/components/Home/FeaturedComp.tsx
+++ src/app/components/Home/FeaturedComp.tsx
@@ -1,7 +1,9 @@
 export default function FeaturedComp() {
   return (
-    <div className="w-full px-[32px] flex items-center flex-col ga-[43px] ">
-      {" "}
+    <div className="w-full px-[32px] flex items-start  flex-col gap-[43px] ">
+      <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[-1.714px]">
+        Featured
+      </h1>
     </div>
   );
 }
