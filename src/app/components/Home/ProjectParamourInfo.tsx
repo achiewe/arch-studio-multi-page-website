528416126e929128e741a154baffbf84cdb25857
achiewe <mamukashviliachi706@gmail.com>
2023-10-27 16:05:10 +0400

    add margin bottom projectParamourInfo component

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function ProjectParamourInfo(): JSX.Element {
   return (
-    <div className="w-full h-[560px] flex bg-heroPattern bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative bg-PraramourMob">
+    <div className="w-full h-[560px] flex bg-heroPattern bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative bg-PraramourMob mb-[141px]">
       <div className="w-full flex flex-col gap-[11px] items-start">
         <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10">
           Project Paramour
