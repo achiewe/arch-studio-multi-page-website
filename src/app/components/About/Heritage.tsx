5af94f32deb6375c1576810f21b04f3e20f306e7
achiewe <mamukashviliachi706@gmail.com>
2023-10-28 21:46:51 +0400

    add margin bottom

--- src/app/components/About/Heritage.tsx
+++ src/app/components/About/Heritage.tsx
@@ -1,6 +1,6 @@
 export default function Heritage() {
   return (
-    <div className="flex flex-col w-full px-[32px] items-start gap-[68px]">
+    <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[112px]">
       <div className="w-[65px] h-[1px] bg-[#C8CCD8]"></div>
       <div className="w-full flex flex-col gap-[22px] items-start">
         <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px]">
