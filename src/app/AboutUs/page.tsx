b1f81984f4b58ed0d8ef8f32644a2a10b6264600
achiewe <mamukashviliachi706@gmail.com>
2023-10-28 21:32:00 +0400

    add div for heritage component

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -1,5 +1,11 @@
 import AboutInfo from "../components/About/AboutInfo";
+import Heritage from "../components/About/Heritage";
 
 export default function () {
-  return <AboutInfo />;
+  return (
+    <div>
+      <AboutInfo />;
+      <Heritage />;
+    </div>
+  );
 }
--- src/app/components/About/Heritage.tsx
+++ src/app/components/About/Heritage.tsx
@@ -1,3 +1,7 @@
 export default function Heritage() {
-  return <div className="flex flex-col px-[32px] items-start "> </div>;
+  return (
+    <div className="flex flex-col w-full px-[32px] items-start gap-[68px]">
+      <div className="w-[65px] h-[1px] bg-[#C8CCD8]"></div>
+    </div>
+  );
 }
