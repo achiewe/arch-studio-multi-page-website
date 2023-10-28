a8d76f6eac16fe24648e46ad36c4f079a3e3abef
achiewe <mamukashviliachi706@gmail.com>
2023-10-28 21:54:49 +0400

    create new component Leaders

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -1,11 +1,13 @@
 import AboutInfo from "../components/About/AboutInfo";
 import Heritage from "../components/About/Heritage";
+import Leaders from "../components/About/Leaders";
 
 export default function () {
   return (
     <div>
       <AboutInfo />
       <Heritage />
+      <Leaders />
     </div>
   );
 }
---
+++ src/app/components/About/Leaders.tsx
@@ -0,0 +1,3 @@
+export default function Leaders() {
+  return <div className="w-full flex flex-col px-[32px] gap-[55px] "> </div>;
+}
