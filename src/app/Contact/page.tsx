6d410d5ebe8fc79a89e8c4804b9bee007c61a9f5
achiewe <mamukashviliachi706@gmail.com>
2023-10-29 12:00:50 +0400

    add office two information in component

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,11 @@
 import AboutProj from "../components/Contact/AboutProj";
+import ContactDetails from "../components/Contact/ContactDetails";
 
 export default function () {
   return (
     <>
       <AboutProj />
+      <ContactDetails />
     </>
   );
 }
