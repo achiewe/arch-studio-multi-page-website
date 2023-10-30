d09d1ece3747ecf9b8686c7b4e9be7f1c2e83876
achiewe <mamukashviliachi706@gmail.com>
2023-10-30 17:01:03 +0400

    add logic about open frame in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Providers } from "@/features/Provider";
 import Header from "./components/Header/Header";
 import BurgerMenu from "./components/Header/BurgerMenu";
 import Footer from "./components/Footer/Footer";
+import { useSelector } from "react-redux";
+import { RootState } from "@/features/store";
 
 const spartan = League_Spartan({ subsets: ["latin"], weight: ["500", "700"] });
 
@@ -19,14 +21,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const openFrame = useSelector(
+    (store: RootState) => store.openFrame.openFrame
+  );
+
   return (
     <html lang="en">
       <Providers>
         <body className={spartan.className}>
+          <div
+            className={`absolute w-full h-full bg-[#000000] opacity-50 z-20 ${
+              openFrame ? "flex" : "none"
+            }`}
+          ></div>
           <Header />
           <BurgerMenu />
           {children}
-
           <Footer />
         </body>
       </Providers>
