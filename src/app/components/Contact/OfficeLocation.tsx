e6e12491f0976e3376f15801da4f7fb67b32dbb6
achiewe <mamukashviliachi706@gmail.com>
2023-10-31 17:14:37 +0400

    add width full for responsive design

--- src/app/components/Contact/OfficeLocation.tsx
+++ src/app/components/Contact/OfficeLocation.tsx
@@ -22,7 +22,7 @@ export default function OfficeLocation() {
   });
 
   return (
-    <div className="w-[375px] h-[367px] mb-[73px]">
+    <div className="w-full h-[367px] mb-[73px]">
       <MapContainer
         center={[34.756846, -90.068075]}
         zoom={5}
