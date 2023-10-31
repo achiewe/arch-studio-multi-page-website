219946b138ef811750d4dbcd1f3b92ffd38a77cc
achiewe <mamukashviliachi706@gmail.com>
2023-10-31 16:20:30 +0400

    add map on the contacts page

--- src/app/components/Contact/OfficeLocation.tsx
+++ src/app/components/Contact/OfficeLocation.tsx
@@ -1,8 +1,43 @@
 "use client";
-
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
 
 export default function OfficeLocation() {
-  return <div className="w-full h-[367px]"></div>;
+  const markers = [
+    {
+      geocode: [32.195189, -95.852155],
+      popUp: "first office",
+    },
+
+    {
+      geocode: [35.432421, -83.150185],
+      popUp: "first office",
+    },
+  ];
+
+  const customIcon = new Icon({
+    iconUrl: require("../../../../public/assets/icons/icon-hamburger.svg"),
+    iconSize: [40, 48],
+  });
+
+  return (
+    <div className="w-[375px] h-[367px]">
+      <MapContainer
+        center={[34.756846, -90.068075]}
+        zoom={3}
+        scrollWheelZoom={false}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers.map((marker) => {
+          const [lat, lng] = marker.geocode;
+          return <Marker position={[lat, lng]} icon={customIcon}></Marker>;
+        })}
+      </MapContainer>
+    </div>
+  );
 }
