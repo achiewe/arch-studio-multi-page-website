7d1092f5fbfd0e4f96cd174d5ff4f8ea499858a2
achiewe <mamukashviliachi706@gmail.com>
2023-10-31 14:03:30 +0400

    add mew component for location office

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,12 +1,14 @@
 import AboutProj from "../components/Contact/AboutProj";
 import ConnectWith from "../components/Contact/ConnectWith";
 import ContactDetails from "../components/Contact/ContactDetails";
+import OfficeLocation from "../components/Contact/OfficeLocation";
 
 export default function () {
   return (
     <div>
       <AboutProj />
       <ContactDetails />
+      <OfficeLocation />
       <ConnectWith />
     </div>
   );
