72fd10a72460dada1b045a5024a771f6481a5eb8
achiewe <mamukashviliachi706@gmail.com>
2023-11-01 14:22:04 +0400

    add Links in responsive tablet design

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import logoSvg from "../../../../public/assets/logo.svg";
 import burgerSvg from "../../../../public/assets/icons/icon-hamburger.svg";
 import { useDispatch } from "react-redux";
 import { setOpenFrame } from "@/features/OpenBurgerSlice";
+import Link from "next/link";
 
 export default function Header(): JSX.Element {
   //use dispatch for redux
@@ -11,20 +12,40 @@ export default function Header(): JSX.Element {
 
   //header component
   return (
-    <header className="flex flex-row p-[32px] justify-between w-full items-center ">
+    <header className="flex flex-row p-[32px] justify-between w-full items-center md:gap-[79px] md:justify-center md:py-[56px] md:px-0 ">
       <img
         src={logoSvg.src}
         alt="logo svg"
-        className="w-[77px] h-[32.028px] cursor-pointer"
+        className="w-[77px] h-[32.028px] cursor-pointer md:w-[96px] md:h-[40px]"
       />
       <img
-        className="cursor-pointer"
+        className="cursor-pointer md:hidden"
         src={burgerSvg.src}
         alt="burger svg"
         onClick={() => {
           dispatch(setOpenFrame());
         }}
       />
+      <div className="hidden md:flex flex-row items-center gap-[51px]">
+        <Link
+          href="/Portfolio"
+          className="text-[#7D828F] font-bold text-[18px] leading-[25px] cursor-pointer"
+        >
+          Portfolio
+        </Link>
+        <Link
+          href="/AboutUs"
+          className="text-[#7D828F] font-bold text-[18px] leading-[25px] cursor-pointer"
+        >
+          About Us
+        </Link>
+        <Link
+          href="/Contact"
+          className="text-[#7D828F] font-bold text-[18px] leading-[25px] cursor-pointer"
+        >
+          Contact
+        </Link>
+      </div>
     </header>
   );
 }
