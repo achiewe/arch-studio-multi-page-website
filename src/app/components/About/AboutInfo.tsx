01f3cdb94ca1346c424f7fe3c3cf8bff48f32632
achiewe <mamukashviliachi706@gmail.com>
2023-11-01 20:55:37 +0400

    display none white div when will be tablet responsive design

--- src/app/components/About/AboutInfo.tsx
+++ src/app/components/About/AboutInfo.tsx
@@ -3,7 +3,7 @@ export default function AboutInfo() {
     <div className="w-full flex flex-col gap-[20px] mb-[76px]">
       <div className="w-full h-[240px] flex items-end justify-start bg-imageHeroMob bg-no-repeat bg-cover relative">
         <div className="w-full h-full bg-black opacity-50 top-0 left-0 right-0 bottom-0 absolute"></div>
-        <div className="w-[345px] h-[45px] bg-[#FFFFFF] z-10 flex "></div>
+        <div className="w-[345px] h-[45px] bg-[#FFFFFF] z-10 flex md:hidden "></div>
       </div>
       <div className="flex w-full px-[32px] flex-col gap-[21px] ">
         <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[-1.2px]">
