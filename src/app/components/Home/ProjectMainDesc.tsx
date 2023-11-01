f6f09f09e2800a640a0a9baec3e6d10f40a4f857
achiewe <mamukashviliachi706@gmail.com>
2023-11-01 17:44:48 +0400

    finish responsive tablet design for this component

--- src/app/components/Home/ProjectMainDesc.tsx
+++ src/app/components/Home/ProjectMainDesc.tsx
@@ -1,25 +1,31 @@
 export default function ProjectMainDesc(): JSX.Element {
   return (
-    <div className="w-full flex flex-col gap-[22px] px-[32px] items-start mb-[113px]">
-      <h1 className="text-[48px] font-bold text-[#1B1D23] leading-[52px] tracking-[-1.714px]">
+    <div className="w-full flex flex-col gap-[22px] px-[32px] items-start mb-[113px] relative md:gap-[43px] md:w-[572px] md:px-0 md:mb-[233px]">
+      <h3 className="hidden md:flex absolute top-[-150px] w-[581px] text-[120px] font-bold text-[#EEEFF4] leading-[200px] tracking-[-3px]">
+        Welcome
+      </h3>
+      <h1 className="text-[48px] font-bold text-[#1B1D23] leading-[52px] tracking-[-1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[446px]">
         Welcome to Arch Studio
       </h1>
-      <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-        We have a unique network and skillset to help bring your projects to
-        life. Our small team of highly skilled individuals combined with our
-        large network put us in a strong position to deliver exceptional
-        results.
-      </p>
-      <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-        Over the past 10 years, we have worked on all kinds of projects. From
-        stations to high-rise buildings, we create spaces that inspire and
-        delight.
-      </p>
-      <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
-        We work closely with our clients so that we understand the intricacies
-        of each project. This allows us to work in harmony the surrounding area
-        to create truly stunning projects that will stand the test of time.
-      </p>
+      <div className="flex flex-col gap-[22px] md:w-[572px]">
+        <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
+          We have a unique network and skillset to help bring your projects to
+          life. Our small team of highly skilled individuals combined with our
+          large network put us in a strong position to deliver exceptional
+          results.
+        </p>
+        <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
+          Over the past 10 years, we have worked on all kinds of projects. From
+          stations to high-rise buildings, we create spaces that inspire and
+          delight.
+        </p>
+        <p className="text-[18px] font-medium text-[#60636D] leading-[24px]">
+          We work closely with our clients so that we understand the intricacies
+          of each project. This allows us to work in harmony the surrounding
+          area to create truly stunning projects that will stand the test of
+          time.
+        </p>
+      </div>
     </div>
   );
 }
