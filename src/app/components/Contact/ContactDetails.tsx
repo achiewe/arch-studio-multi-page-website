480076fbef0cdc5a6c25647ba10c38c89970c738
achiewe <mamukashviliachi706@gmail.com>
2023-11-02 18:07:21 +0400

    add responsive tablet design on the view map and location info

--- src/app/components/Contact/ContactDetails.tsx
+++ src/app/components/Contact/ContactDetails.tsx
@@ -2,13 +2,13 @@ import ArrowSvg from "../../../../public/assets/icons/icon-arrow.svg";
 
 export default function ContactDetails() {
   return (
-    <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[71px]">
+    <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[71px] md:gap-[49px] md:px-0 md:w-[573px]">
       <div className="w-[65px] h-[1px] bg-[#C8CCD8]"></div>
-      <div className="w-full flex flex-col gap-[40px] items-start">
-        <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px]">
+      <div className="w-full flex flex-col gap-[40px] items-start md:gap-[42px]">
+        <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[300px]">
           Contact Details
         </h1>
-        <div className="flex flex-col gap-[43px]">
+        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full">
           <div className="flex flex-col gap-[14px]">
             <p className="text-[18px] text-[#60636D] fontbold leading-[35px]">
               Main Office
@@ -34,7 +34,7 @@ export default function ContactDetails() {
           </a>
         </div>
 
-        <div className="flex flex-col gap-[43px]">
+        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full">
           <div className="flex flex-col gap-[14px]">
             <p className="text-[18px] text-[#60636D] fontbold leading-[35px]">
               Office II
