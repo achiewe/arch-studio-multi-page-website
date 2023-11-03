efa920f3b2b8150c44bf445818b2c466f16ee6bd
achiewe <mamukashviliachi706@gmail.com>
2023-11-03 13:29:56 +0400

    add on the footer div max width and also on the Link

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 //add footer component
 export default function Footer(): JSX.Element {
   return (
-    <footer className="bg-[#EEEFF4] w-full flex flex-col items-center gap-[33px] pb-[48px] md:flex-row md:pb-0 md:w-[79%] md:gap-[40px] md:relative lg:w-[984px]">
-      <img src={archSvg.src} alt="arch svg" className="mt-[-60px] md:mt-0" />
+    <footer className="bg-[#EEEFF4] w-full flex flex-col items-center gap-[33px] pb-[48px] md:flex-row md:pb-0 md:w-[79%] md:gap-[40px] md:relative lg:max-w-[984px] lg:gap-[85px]">
+      <img
+        src={archSvg.src}
+        alt="arch svg"
+        className="mt-[-60px] md:mt-0 lg:w-[200px] lg:h-[200px]"
+      />
       <nav className="flex flex-col gap-[32px] items-center md:flex-row md:gap-[51px]">
         <Link
           href="/Portfolio"
@@ -28,7 +32,7 @@ export default function Footer(): JSX.Element {
       </nav>
       <Link
         href="/Portfolio"
-        className="w-[252px] h-[72px] bg-[#1B1D23] pt-[25px] pb-[22px] pl-[37px] pr-[32px] flex items-center gap-[24px] md:absolute md:right-[-25.5%]"
+        className="w-[252px] h-[72px] bg-[#1B1D23] pt-[25px] pb-[22px] pl-[37px] pr-[32px] flex items-center gap-[24px] md:absolute md:right-[-25.5%] lg:right-[-15%] lg:max-w-[252px]"
       >
         <h3 className="text-[#fff] font-bold text-[18px] leading-[25px] cursor-pointer ">
           See Our Portfolio
