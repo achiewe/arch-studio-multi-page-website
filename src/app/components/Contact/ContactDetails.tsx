4f851fd87a4c5ebb037901f99f347957953f71ce
achiewe <mamukashviliachi706@gmail.com>
2023-11-06 17:19:07 +0400

    add office correct position on the site

--- src/app/components/Contact/ContactDetails.tsx
+++ src/app/components/Contact/ContactDetails.tsx
@@ -5,11 +5,11 @@ export default function ContactDetails() {
     <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[71px] md:gap-[49px] md:px-0 md:w-[573px] md:mb-[226px] lg:w-[1110px] lg:flex-row">
       <hr className="w-[65px] h-[1px] bg-[#C8CCD8] border-none lg:hidden" />
       <div className="w-full flex flex-col gap-[40px] items-start md:gap-[42px] lg:flex-row">
-        <hr className="hidden lg:flex w-[65px] h-[1px] bg-[#C8CCD8] border-none" />
+        <hr className="hidden lg:flex w-[65px] h-[1px] bg-[#C8CCD8]" />
         <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[300px]">
           Contact Details
         </h1>
-        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full">
+        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full lg:flex-col lg:gap-[59px] lg:items-start">
           <div className="flex flex-col gap-[14px]">
             <p className="text-[18px] text-[#60636D] fontbold leading-[35px]">
               Main Office
@@ -35,7 +35,7 @@ export default function ContactDetails() {
           </a>
         </div>
 
-        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full">
+        <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full lg:flex-col lg:gap-[59px] lg:items-start">
           <div className="flex flex-col gap-[14px]">
             <p className="text-[18px] text-[#60636D] fontbold leading-[35px]">
               Office II
