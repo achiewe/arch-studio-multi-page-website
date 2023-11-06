3c37b6ef949c9058960032f2949bc5da92df1c6a
achiewe <mamukashviliachi706@gmail.com>
2023-11-06 17:58:49 +0400

    add gap between title and office info

--- src/app/components/Contact/ContactDetails.tsx
+++ src/app/components/Contact/ContactDetails.tsx
@@ -2,13 +2,14 @@ import ArrowSvg from "../../../../public/assets/icons/icon-arrow.svg";
 
 export default function ContactDetails() {
   return (
-    <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[71px] md:gap-[49px] md:px-0 md:w-[573px] md:mb-[226px] lg:w-[1110px] lg:flex-row">
-      <hr className="w-[65px] h-[1px] bg-[#C8CCD8] border-none lg:hidden" />
-      <div className="w-full flex flex-col gap-[40px] items-start md:gap-[42px] lg:flex-row">
-        <hr className="hidden lg:flex w-[65px] h-[1px] bg-[#C8CCD8]" />
-        <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[300px]">
-          Contact Details
-        </h1>
+    <div className="flex flex-col w-full px-[32px] items-start gap-[68px] mb-[71px] md:gap-[49px] md:px-0 md:w-[573px] md:mb-[226px] lg:w-[1110px] lg:gap-[73px]">
+      <hr className="w-[65px] h-[1px] bg-[#C8CCD8] border-none" />
+      <div className="w-full flex flex-col gap-[40px] items-start md:gap-[42px] lg:flex-row lg:gap-[30px]">
+        <div className="flex flex-col lg:gap-[73px]">
+          <h1 className="text-[48px] text-[#1B1D23] font-bold leading-[52px] tracking-[ -1.714px] md:text-[72px] md:leading-[64px] md:tracking-[-2px] md:w-[300px] lg:w-[350px]">
+            Contact Details
+          </h1>
+        </div>
         <div className="flex flex-col gap-[43px] md:flex-row md:justify-between md:gap-0 md:items-end md:w-full lg:flex-col lg:gap-[59px] lg:items-start">
           <div className="flex flex-col gap-[14px]">
             <p className="text-[18px] text-[#60636D] fontbold leading-[35px]">
