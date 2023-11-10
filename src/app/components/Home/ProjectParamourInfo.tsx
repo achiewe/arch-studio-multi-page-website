5a16467d35c9733f20008fe0d3fd1030410d59ee
achiewe <mamukashviliachi706@gmail.com>
2023-11-10 17:57:15 +0400

    add responsive desktop design for projectParamourInfo component

--- src/app/components/Home/ProjectParamourInfo.tsx
+++ src/app/components/Home/ProjectParamourInfo.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function ProjectParamourInfo(): JSX.Element {
   return (
-    <div className="w-full h-[560px] flex bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative bg-PraramourMob mb-[141px] md:w-[573px] md:h-[720px] md:bg-ParamourTab md:px-[58px] md:gap-[41px] md:mb-[248px]">
+    <div className="w-full h-[560px] flex bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative bg-PraramourMob mb-[141px] md:w-[573px] md:h-[720px] md:bg-ParamourTab md:px-[58px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]">
       <div className="w-full flex flex-col gap-[11px] items-start md:gap-[10px]">
-        <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10 md:text-[96px] md:leading-[80px]">
+        <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10 md:text-[96px] md:leading-[80px] lg:w-[544px]">
           Project Paramour
         </h1>
-        <p className="text-[18px] text-[#FFF] font-medium leading-[24px] z-10">
+        <p className="text-[18px] text-[#FFF] font-medium leading-[24px] z-10 lg:w-[445px]">
           Project made for an art museum near Southwest London. Project Paramour
           is a statement of bold, modern architecture.
         </p>
