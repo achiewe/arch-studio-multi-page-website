fd75a1c3715e2239b3166cc4ef8ade425ac82105
achiewe <mamukashviliachi706@gmail.com>
2023-11-10 14:43:25 +0400

    add gap between the div image and text div

--- src/app/components/Home/ProjectMainDesc.tsx
+++ src/app/components/Home/ProjectMainDesc.tsx
@@ -2,7 +2,7 @@ import welcomeoImg from "../../../../public/assets/home/desktop/image-welcome.jp
 
 export default function ProjectMainDesc(): JSX.Element {
   return (
-    <div className="w-full flex flex-col gap-[22px] px-[32px] items-start mb-[113px] relative md:gap-[43px] md:w-[572px] md:px-0 md:mb-[233px] lg:flex-row lg:w-[1110px] lg:items-end lg:gap-0 lg:justify-center">
+    <div className="w-full flex flex-col gap-[22px] px-[32px] items-start mb-[113px] relative md:gap-[43px] md:w-[572px] md:px-0 md:mb-[233px] lg:flex-row lg:w-[1110px] lg:items-end lg:justify-center lg:gap-[125px]">
       <h3 className="hidden md:flex absolute top-[-150px] w-[581px] text-[120px] font-bold text-[#EEEFF4] leading-[200px] tracking-[-3px] lg:text-[250px] lg:left-[-150px] lg:top-[-90px]">
         Welcome
       </h3>
