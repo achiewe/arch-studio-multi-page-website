e242007400ba7bceb6c9a381d7c8616ea1110746
achiewe <mamukashviliachi706@gmail.com>
2023-11-11 18:12:32 +0400

    add logic about background image

--- src/app/components/Home/ParamouProjDesk.tsx
+++ src/app/components/Home/ParamouProjDesk.tsx
@@ -29,10 +29,13 @@ export default function ParamouProjDesk() {
   console.log(project, "mevar");
 
   return (
-    <div className="w-full h-[560px] flex bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative bg-PraramourMob mb-[141px] md:w-[573px] md:h-[720px] md:bg-ParamourTab md:px-[58px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]">
+    <div
+      className="w-full h-[560px] flex bg-no-repeat bg-cover px-[32px] justify-center items-start flex-col gap-[83px] relative mb-[141px] md:w-[573px] md:h-[720px] md:px-[58px] md:gap-[41px] md:mb-[248px] lg:w-[1110px] lg:px-[190px]"
+      style={{ backgroundImage: `url(${project[0].image})` }}
+    >
       <div className="w-full flex flex-col gap-[11px] items-start md:gap-[10px]">
         <h1 className="text-[48px] text-[#FFF] font-bold leading-[48px] tracking-[-1.2px] z-10 md:text-[96px] md:leading-[80px] lg:w-[544px]">
-          Project Paramour
+          {project[0].name}
         </h1>
         <p className="text-[18px] text-[#FFF] font-medium leading-[24px] z-10 lg:w-[445px]">
           Project made for an art museum near Southwest London. Project Paramour
