ee944096d2c99176bf58cc3430540b885b466292
achiewe <mamukashviliachi706@gmail.com>
2023-11-11 23:24:01 +0400

    add item image in backgorund image

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -3,45 +3,13 @@ import { useEffect, useState } from "react";
 import { PortfolioItems } from "./PortfolioData";
 
 export default function PortfComp() {
-  const [currentDeviceType, updateDeviceType] = useState(detectDeviceType());
-
-  useEffect(() => {
-    function handleWindowResize() {
-      updateDeviceType(detectDeviceType());
-    }
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
-
-  function detectDeviceType() {
-    const windowWidth = window.innerWidth;
-
-    if (windowWidth <= 768) {
-      return "mobile";
-    } else if (windowWidth <= 1024) {
-      return "tablet";
-    } else {
-      return "desktop";
-    }
-  }
   return (
     <div className="mt-[55px] flex flex-col gap-[24px] px-[32px] w-full mb-[132px] md:mt-0 md:mb-[200px] md:px-0 justify-center items-center lg:max-w-[1110px] lg:flex-row lg:flex-wrap lg:gap-x-[30px] lg:gap-y-[32px]">
       {PortfolioItems.PortfolioArray.map((item, id) => {
-        const imgPath =
-          currentDeviceType === "mobile"
-            ? item.image
-            : currentDeviceType === "tablet"
-            ? item.image1
-            : item.image2;
         return (
           <div
             key={id}
-            className="w-full h-[240px] pl-[24px] pb-[23px] flex flex-col justify-end bg-no-repeat bg-cover relative md:w-[573px] md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px]"
-            style={{ backgroundImage: `url(${imgPath})` }}
+            className={`bg-[url('${item.image}')] w-full h-[240px] pl-[24px] pb-[23px] flex flex-col justify-end bg-no-repeat bg-cover relative md:w-[573px] md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px]`}
           >
             <h2 className="text-[32px] text-[#FFF] font-bold leading-[40px] z-10">
               {item.name}
