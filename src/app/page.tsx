6cd1a43ced120eeeaffd17bd1562e0b5a6a5712e
achiewe <mamukashviliachi706@gmail.com>
2023-11-11 17:16:56 +0400

    create new component and also data for map the main page items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutTeam from "./components/Home/AboutTeam";
 import ProjectMainDesc from "./components/Home/ProjectMainDesc";
 import ProjectParamouInfo from "./components/Home/ProjectParamourInfo";
+import ParamouProjDesk from "./components/Home/ParamouProjDesk";
 import FeaturedComp from "./components/Home/FeaturedComp";
 export default function Home() {
   // home page component
   return (
     <div className="flex flex-col w-full relative md:items-center md:justify-center">
       <ProjectParamouInfo />
+      <ParamouProjDesk />
       <ProjectMainDesc />
       <AboutTeam />
       <FeaturedComp />
