299f045135d546e94266b6ea059a78e6f36f0635
achiewe <mamukashviliachi706@gmail.com>
2023-11-14 17:57:45 +0400

    change portfolio page tsx component code

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -1,39 +1,47 @@
 "use client";
+import { useEffect, useState } from "react";
 import { PortfolioItems } from "./PortfolioData";
 
 export default function PortfComp() {
+  const [currentDeviceType, updateDeviceType] = useState(detectDeviceType());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      updateDeviceType(detectDeviceType());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function detectDeviceType() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 768) {
+      return "mobile";
+    } else if (windowWidth <= 1024) {
+      return "tablet";
+    } else {
+      return "desktop";
+    }
+  }
   return (
-    <div className="mt-[55px] flex flex-col gap-[24px] px-[32px] w-full mb-[132px] md:mt-0 md:mb-[200px] md:px-0 justify-center items-center lg:w-[1110px] lg:flex-row lg:flex-wrap lg:gap-x-[30px] lg:gap-y-[32px]">
-      {PortfolioItems.PortfolioArray.map((item, id) => (
-        <div
-          key={id}
-          className="flex flex-col w-full justify-center items-center lg:flex-row lg:gap-x-[30px] lg:gap-y-[32px] lg:w-[1110px]"
-        >
-          <div
-            style={{
-              background: `url(${item.image})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-            className={`w-full h-[240px] pl-[24px] pb-[23px] flex flex-col justify-end bg-no-repeat bg-cover relative md:w-[573px] md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px] md:hidden`}
-          >
-            <h2 className="text-[32px] text-[#FFF] font-bold leading-[40px] z-10">
-              {item.name}
-            </h2>
-            <span className="text-[18px] w-[125px] cursor-pointer opacity-75 text-[#FFF] font-medium leading-[24px] z-10">
-              {item.date}
-            </span>
-            <div className="w-full h-full bg-black opacity-50 top-0 left-0 right-0 bottom-0 absolute"></div>
-          </div>
-
+    <div className="mt-[55px] flex flex-col gap-[24px] px-[32px] w-full mb-[132px] md:mt-0 md:mb-[200px] md:px-0 justify-center items-center lg:max-w-[1110px] lg:flex-row lg:flex-wrap lg:gap-x-[30px] lg:gap-y-[32px]">
+      {PortfolioItems.PortfolioArray.map((item, id) => {
+        const imgPath =
+          currentDeviceType === "mobile"
+            ? item.image
+            : currentDeviceType === "tablet"
+            ? item.image1
+            : item.image2;
+        return (
           <div
             key={id}
-            style={{
-              background: `url(${item.image1})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-            className={`hidden md:flex lg:hidden w-full h-[240px] pl-[24px] pb-[23px] flex-col justify-end bg-no-repeat bg-cover relative md:w-[573px] md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px] `}
+            className="w-full h-[240px] pl-[24px] pb-[23px] flex flex-col justify-end bg-no-repeat bg-cover relative md:w-[573px] md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px]"
+            style={{ backgroundImage: `url(${imgPath})` }}
           >
             <h2 className="text-[32px] text-[#FFF] font-bold leading-[40px] z-10">
               {item.name}
@@ -43,26 +51,8 @@ export default function PortfComp() {
             </span>
             <div className="w-full h-full bg-black opacity-50 top-0 left-0 right-0 bottom-0 absolute"></div>
           </div>
-
-          <div
-            key={id}
-            style={{
-              background: `url(${item.image2})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-            className={`hidden md:hidden lg:flex w-full h-[240px] pl-[24px] pb-[23px] flex-col justify-end bg-no-repeat bg-cover relative md:pl-[40px] md:pb-[39px] lg:w-[350px] lg:h-[560px]`}
-          >
-            <h2 className="text-[32px] text-[#FFF] font-bold leading-[40px] z-10">
-              {item.name}
-            </h2>
-            <span className="text-[18px] w-[125px] cursor-pointer opacity-75 text-[#FFF] font-medium leading-[24px] z-10">
-              {item.date}
-            </span>
-            <div className="w-full h-full bg-black opacity-50 top-0 left-0 right-0 bottom-0 absolute"></div>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
