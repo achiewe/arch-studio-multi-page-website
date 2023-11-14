3bc4f9fda2c2e3e9c9578ef17caefa4a2049e7d3
achiewe <mamukashviliachi706@gmail.com>
2023-11-14 17:26:02 +0400

    renew component and add again contact components and which should render contact page.tsx

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,15 @@
+import AboutProj from "../components/Contact/AboutProj";
+import ConnectWith from "../components/Contact/ConnectWith";
+import ContactDetails from "../components/Contact/ContactDetails";
+import OfficeLocation from "../components/Contact/OfficeLocation";
+
+export default function ContactComp() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <AboutProj />
+      <ContactDetails />
+      <OfficeLocation />
+      <ConnectWith />
+    </div>
+  );
+}
