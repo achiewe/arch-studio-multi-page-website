c060dcf6aa3affa90593724ad4fa5f0a3b7d5bb1
achiewe <mamukashviliachi706@gmail.com>
2023-11-14 17:28:01 +0400

    remove officelocation from the components

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,14 +1,14 @@
 import AboutProj from "../components/Contact/AboutProj";
 import ConnectWith from "../components/Contact/ConnectWith";
 import ContactDetails from "../components/Contact/ContactDetails";
-import OfficeLocation from "../components/Contact/OfficeLocation";
+// import OfficeLocation from "../components/Contact/OfficeLocation";
 
 export default function ContactComp() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <AboutProj />
       <ContactDetails />
-      <OfficeLocation />
+      {/* <OfficeLocation /> */}
       <ConnectWith />
     </div>
   );
