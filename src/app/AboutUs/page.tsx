a94bafd201ef06de78ca79a0321ecdfc474ca337
achiewe <mamukashviliachi706@gmail.com>
2023-11-17 20:57:55 +0400

    add comment in the page tsx

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -2,6 +2,7 @@ import AboutInfo from "../components/About/AboutInfo";
 import Heritage from "../components/About/Heritage";
 import Leaders from "../components/About/Leaders";
 
+// about us main component
 export default function aboutUs() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
