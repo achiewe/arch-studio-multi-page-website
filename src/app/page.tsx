8c610dd5464a597620d7bb0018472932af79b96a
achiewe <mamukashviliachi706@gmail.com>
2023-12-03 16:41:39 +0400

    add new comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProjectParamouInfo from "./components/Home/ProjectParamourInfo";
 import ParamouProjDesk from "./components/Home/ParamouProjDesk";
 import FeaturedComp from "./components/Home/FeaturedComp";
 export default function Home() {
-  // home page component
+  // home page component for main page
   return (
     <div className="flex flex-col w-full relative md:items-center md:justify-center">
       <ProjectParamouInfo />
